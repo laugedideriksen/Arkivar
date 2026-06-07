4f4b5992ebddbb84f8c08fdf9c0b7e9d63d16748
ld <dideriksen@proton.me>
2026-06-07 22:03:23 +0200

    Changelog sketch added.

--- archivists_helper.py
+++ archivists_helper.py
@@ -5,11 +5,14 @@ import pathlib
 import bagit
 from datetime import datetime
 
+
 # CHANGELOG
 class LogWriter:
-    def __init__(self, project_name:str, dt_format:str = '%Y%m%d-%H:%M:%S.%f')->None:
+    def __init__(
+        self, project_name: str, dt_format: str = "%Y%m%d-%H:%M:%S.%f"
+    ) -> None:
         self.project_name = project_name
-        self.log_file = self.project_name + 'changelog.csv'
+        self.log_file = self.project_name + "changelog.csv"
         self.dt_format = dt_format
 
     def calculate_sha256(self, file_path):
@@ -19,35 +22,58 @@ class LogWriter:
             for byte_block in iter(lambda: f.read(4096), b""):
                 sha256().update(byte_block)
             return sha256().hexdigest()
-                
-# TODO: Create CSV
-    def csv_writer(self, action_type, path_before, path_after, file_hash=None):
+
+    # Create and write to CSV
+    def csv_writer(
+        self,
+        action_type: str,
+        path_before: str,
+        path_after: str,
+        hash_before: str = "N/A",
+    ):
         file_exists = os.path.isfile(self.log_file)
 
-        with open(self.log_file, mode='a', newline='') as f:
+        with open(self.log_file, mode="a", newline="") as f:
             writer = csv.writer(f)
             if not file_exists:
-                writer.writerow(['timestamp', 'action_type', 'file_path_before', 'file_path_after', 'hash_before', 'hash_after'])
+                writer.writerow(
+                    [
+                        "timestamp",
+                        "action_type",
+                        "file_path_before",
+                        "file_path_after",
+                        "hash_before",
+                        "hash_after",
+                    ]
+                )
                 f.flush()
-                writer.writerow([
-                    datetime.now().strftime('%Y%m%d-%H:%M:%S.%f'), 
-                    'create_changelog', 
-                    None, 
-                    os.path.abspath(self.log_file), 
-                    None
-                    ])
+                writer.writerow(
+                    [
+                        datetime.now().strftime("%Y%m%d-%H:%M:%S.%f"),
+                        "create_changelog",
+                        None,
+                        os.path.abspath(self.log_file),
+                        None,
+                    ]
+                )
                 f.flush()
-            
-            
-# TODO: Create methods for:
-# Ingestion
-# File Validation
-# Metadata Extraction
-# Metadata normalisation
-# Directory creation/manipulation
-# File moving
-# BagIt
-# Regular Checks
+
+            permitted_actions = ['INGEST_FILE', 'COMPUTE_CHECKSUM', 'EXTRACT_METADATA', 'NORMALISE_FILENAME', 'NORMALISE_METADATA', 'CREATE_DIRECTORY', 'MOVE', 'CREATE_BAG', 'VALIDATE_BAG']
+            if action_type not in permitted_actions:
+                action_type = 'ERROR'
+
+            writer.writerow(
+                [
+                    datetime.now().strftime(self.dt_format),
+                    action_type,
+                    path_before,
+                    path_after,
+                    hash_before,
+                    self.calculate_sha256(path_after),
+                ]
+            )
+            f.flush()
+
 
 # ERROR REPORTING
 
@@ -66,8 +92,8 @@ class LogWriter:
 # Create cronjob or similar to check for corruption on a regular basis.
 
 # CLEANUP AND REPORT
-# 
+#
 
 
-if __name__ == '__main__':
-    print(LogWriter("name").calculate_sha256("pypoject.toml"))
+if __name__ == "__main__":
+    pass
