e6dff7d6bae4a81bda8fdefd35c04f1f8681aeb8
ld <dideriksen@proton.me>
2026-06-15 09:47:38 +0200

    File validation implemented and added to ingest method.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Archivist's Helper
 
-## Workflow
-Create directory. Initialise directory. Edit metadata.json.
+## Intended Workflow
+Create directory. Initialise directory; this will create two directories, `staging/` and `quarantine/`, as well as two files, `changelog.csv` and `metadata.json`. If any of these already exits, they will not be overwritten. Edit metadata.json to contain all relevant project information and delete any unused fields.
--- archivists_helper.py
+++ archivists_helper.py
@@ -1,21 +1,20 @@
 import os
 import subprocess
 import shutil
-import pathlib
+from pathlib import Path
 from datetime import datetime
 import csv
 import json
 from hashlib import sha256
+import puremagic
 import bagit
-
 
 # CHANGELOG
 class LogWriter:
     def __init__(
-        self, project_name: str, dt_format: str = "%Y%m%d-%H:%M:%S.%f"
+        self, dt_format: str = "%Y%m%d-%H:%M:%S.%f"
     ) -> None:
-        self.project_name = project_name
-        self.log_file = self.project_name + "_changelog.csv"
+        self.log_file = "changelog.csv"
         self.dt_format = dt_format
 
     def _fail_if_file_exists(self, file_path, name, action) -> bool:
@@ -92,12 +91,14 @@ class LogWriter:
                 "CREATE_CHANGELOG",
                 "CREATE_DUBLIN_CORE_JSON",
                 "INGEST_FILE",
+                "VALIDATE_FILETYPE",
                 "COMPUTE_CHECKSUM",
                 "EXTRACT_METADATA",
                 "NORMALISE_FILENAME",
                 "NORMALISE_METADATA",
                 "CREATE_DIRECTORY",
                 "MOVE",
+                "QUARANTINE_FILE",
                 "CREATE_BAG",
                 "VALIDATE_BAG",
                 "ERROR",
@@ -126,11 +127,23 @@ class LogWriter:
 # Project Initiation
 class ProjectInitiator:
     def __init__(
-        self, project_name: str = "ProjectName", start_date: str = "YYYYmmdd"
+            self, project_directory: str = os.getcwd(), project_name: str = "ProjectName", start_date: str = "YYYYmmdd"
     ) -> None:
+        self.project_directory = project_directory
         self.project_name = project_name
         self.start_date = start_date
-        self.metadata_json = f"{self.project_name}_metadata.json"
+        self.metadata_json = "metadata.json"
+
+    @property
+    def project_directory(self):
+        """The project_directory property."""
+        return self._project_directory
+
+    @project_directory.setter
+    def project_directory(self, value):
+        if not os.path.isdir(value):
+            raise ValueError(f"{value} does not exist or is not a directory.")
+        self._project_directory = value
 
     def _create_dublin_core_json(self) -> None:
         if not LogWriter(self.project_name)._fail_if_file_exists(
@@ -174,7 +187,7 @@ class ProjectInitiator:
             types=["The nature or genre of the resource."],
         )
 
-        with open(f"{self.project_name}_metadata.json", "w") as f:
+        with open("metadata.json", "w") as f:
             json.dump(dc_dict, f, sort_keys=False, indent=4, ensure_ascii=False)
 
         LogWriter(self.project_name)._write_csv(
@@ -185,13 +198,14 @@ class ProjectInitiator:
         )
 
     def init_directory(self):
+        """Initiate archive directory by making a 'staging' and a 'quarantine' directory, creating a blank changelog csv if none exists, and creating a metadata JSON if none exists."""
         LogWriter(self.project_name)._create_csv()
         self._create_dublin_core_json()
         if not os.path.isdir('staging'):
             os.mkdir('staging')
         if not os.path.isdir('quarantine'):
             os.mkdir('quarantine')
-        print("Directory initialised.")
+        print(f"Directory initialised at {os.getcwd()}.")
 
 
 # CREATE DATA SOURCE
@@ -218,10 +232,11 @@ class DataSource:
 # INGESTION
 class DataIngester:
     """This class takes a data source object as its input, and ingests all data from there using rsync."""
-    def __init__(self, data_source):
+    def __init__(self, data_source, destination_directory: str = os.getcwd()):
         self.data_source = data_source
         self.path = self.data_source.source_path
         self.type = self.data_source.source_type
+        self.destination_directory = destination_directory
 
     @property
     def data_source(self):
@@ -243,26 +258,84 @@ class DataIngester:
     def source_path(self, value):
         self._source_path = value
 
-    def _ingest_file(self, ingest_function):
-        res = subprocess.run(ingest_function, capture_output=True)
-        pass
+    @property
+    def destination_directory(self):
+        """The destination_directory property."""
+        return self._destination_directory
 
-    def _ingest_directory(self, ingest_function):
+    @destination_directory.setter
+    def destination_directory(self, value):
+        self._destination_directory = value
+
+    @property
+    def staging_directory(self):
+        """The destination_directory property."""
+        return self._staging_directory
+
+    @staging_directory.setter
+    def staging_directory(self, value):
+        if not os.path.isdir(value + '/staging/'):
+            raise ValueError(f"{value}/staging/ does not exist or is not a directory. Have you initiated the directory?")
+        self._staging_directory = value + '/staging/'
+
+    def _ingest_file(self, ingest_function, destination_subdir):
+        """Ingest a single file"""
+        staged_path = f'{destination_subdir}/{os.path.basename(self.path).split('/')[-1]}'
+        res = subprocess.run(ingest_function, capture_output=True, text=True, encoding="utf8")
+        stdout = res.stdout.replace('\n', '. ')
+
+        if not res.returncode:
+            LogWriter()._write_csv("INGEST_FILE", self.path, staged_path, note=stdout)
+        else:
+            LogWriter()._write_csv("ERROR", self.path, staged_path, note=stdout)
+
+        if Utilities()._validate_file(staged_path):
+            LogWriter()._write_csv("VALIDATE_FILETYPE", staged_path, staged_path, note="File validation succeeded.")
+            # TODO: extract metadata
+        else:
+            LogWriter()._write_csv("VALIDATE_FILETYPE", staged_path, staged_path, note="File validation failed.")
+            Utilities()._quarantine_file(staged_path, f'{self.destination_directory}/quarantine/', "file validation")
+
+    def _ingest_directory(self, ingest_function, destination_subdir):
+        """Walk tree and ingest every file"""
         #TODO: walk tree and call ingest file on every single file.
         pass
 
     def ingest(self, dry_run: bool = False):
+        destination_subdir = f'{self.destination_directory}{datetime.now()}'
         if dry_run:
-            ingest_function = ['rsync','-cain', self.path, 'staging']
+            ingest_function = ['rsync','-cain', self.path, f'{destination_subdir}/']
         else:
-            ingest_function = ['rsync','-cai', self.path, 'staging']
+            ingest_function = ['rsync','-cai', self.path, f'{destination_subdir}/']
 
         if self.type == 'file':
-            self._ingest_file(ingest_function)
+            self._ingest_file(ingest_function, destination_subdir)
         elif self.type == 'directory':
-            self._ingest_directory(ingest_function)
+            self._ingest_directory(ingest_function, destination_subdir)
 
-# FILE VALIDATION
+# UTILITIES: FILE VALIDATION, QUARANTINE
+class Utilities:
+    def __init__(self):
+        pass
+
+    def _validate_file(self, file_path)->bool:
+        magic = puremagic.from_file(file_path)
+        extension = f'.{file_path.rsplit('.', 1)[-1]}'
+        return magic == extension
+
+    def _quarantine_file(self, file_path: str, quarantine_path: str, justification: str)->None:
+        quarantined_path = f'{quarantine_path}/{os.path.basename(file_path).split('/')[-1]}'
+        self._move_no_log(file_path, quarantine_path)
+        LogWriter()._write_csv("QUARANTINE_FILE", file_path, quarantined_path, note=f'This file was quarantined due to failed {justification}.')
+
+    def _move_no_log(self, file_path, destination_directory):
+        if not os.path.isfile(file_path):
+            raise ValueError(f"{file_path} doesn't exist or isn't a file.")
+        elif not os.path.isdir(destination_directory):
+            raise ValueError(f"{destination_directory} doesn't exist or isn't a directory.")
+        filename = os.path.basename(file_path).split('/')[-1]
+        shutil.move(file_path, f'{destination_directory}/{filename}')
+
 
 # METADATA
 
@@ -276,5 +349,6 @@ class DataIngester:
 #
 
 if __name__ == "__main__":
-    ProjectInitiator("Test", "20260608").init_directory()
-    #print(DataIngester(DataSource('cli.py')).path)
+    ProjectInitiator().init_directory()
+    DataIngester(DataSource('cli.py')).ingest()
+    DataIngester(DataSource('during2001.jpg')).ingest()
